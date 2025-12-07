248eec8337e62ee147d4a5a602deb7da188cf3a4
Andrew Babbitt <andrew.babbitt@outlook.com>
2025-12-07 02:29:58 -0700

    chore: update configuration

--- src/Core/Configuration.cs
+++ src/Core/Configuration.cs
@@ -22,12 +22,37 @@ public class Configuration : IPluginConfiguration
     /// <summary>
     /// Gets or sets the network address and port of the Archipelago server to connect to.
     /// </summary>
-    public string ArchipelagoHost { get; set; } = "archipelago.gg:12345";
+    public string Host { get; set; } = "archipelago.gg:12345";
 
     /// <summary>
     /// Gets or sets the name of the Archipelago slot.
     /// </summary>
-    public string ArchipelagoSlotName { get; set; } = string.Empty;
+    public string Slot { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether chat messages are displayed to the user.
+    /// </summary>
+    public bool DisplayChatMessages { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether found hint messages should be displayed to the user.
+    /// </summary>
+    public bool DisplayFoundHintMessages { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether join and leave messages are displayed to the user.
+    /// </summary>
+    public bool DisplayJoinLeaveMessages { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether item received messages should be displayed to the user.
+    /// </summary>
+    public bool DisplayItemReceivedMessages { get; set; } = true;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether item sent messages should be displayed to the user.
+    /// </summary>
+    public bool DisplayItemSentMessages { get; set; }
 
     /// <summary>
     /// Saves the current configuration to persistent storage.

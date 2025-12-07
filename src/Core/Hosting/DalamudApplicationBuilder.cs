cd1e50d952d16451025543951484003bbb3c1ef4
Andrew Babbitt <andrew.babbitt@outlook.com>
2025-12-06 19:05:06 -0700

    feat: hosting

---
+++ src/Core/Hosting/DalamudApplicationBuilder.cs
@@ -0,0 +1,71 @@
+﻿// Licensed to the Archipendium Contributors under one or more agreements.
+// The Archipendium Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Dalamud.Plugin;
+using Dalamud.Plugin.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.Metrics;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Archipendium.Core.Hosting;
+
+/// <summary>
+/// Provides a builder for configuring and constructing a Dalamud host application, including services, configuration, logging, and environment settings.
+/// </summary>
+public sealed class DalamudApplicationBuilder : IHostApplicationBuilder
+{
+    private readonly HostApplicationBuilder _hostApplicationBuilder;
+
+    /// <inheritdoc/>
+    public IDictionary<object, object> Properties => ((IHostApplicationBuilder)_hostApplicationBuilder).Properties;
+
+    /// <inheritdoc/>
+    public IConfigurationManager Configuration => _hostApplicationBuilder.Configuration;
+
+    /// <inheritdoc/>
+    public IHostEnvironment Environment => _hostApplicationBuilder.Environment;
+
+    /// <inheritdoc/>
+    public ILoggingBuilder Logging => _hostApplicationBuilder.Logging;
+
+    /// <inheritdoc/>
+    public IMetricsBuilder Metrics => _hostApplicationBuilder.Metrics;
+
+    /// <inheritdoc/>
+    public IServiceCollection Services => _hostApplicationBuilder.Services;
+
+    internal DalamudApplicationBuilder(IDalamudPluginInterface pluginInterface)
+    {
+        _hostApplicationBuilder = Host.CreateEmptyApplicationBuilder(new()
+        {
+            ApplicationName = pluginInterface.InternalName,
+            ContentRootPath = pluginInterface.AssemblyLocation.DirectoryName,
+            EnvironmentName = pluginInterface.IsDev ? Environments.Development : Environments.Production
+        });
+
+        AddDefaultServices(pluginInterface, _hostApplicationBuilder.Services);
+    }
+
+    /// <inheritdoc/>
+    public void ConfigureContainer<TContainerBuilder>(IServiceProviderFactory<TContainerBuilder> factory, Action<TContainerBuilder>? configure = null) where TContainerBuilder : notnull
+    {
+        _hostApplicationBuilder.ConfigureContainer(factory, configure);
+    }
+
+    /// <summary>
+    /// Builds and returns a configured instance of the <see cref="DalamudApplication"/> based on the current application builder settings.
+    /// </summary>
+    /// <returns>A <see cref="DalamudApplication"/> instance initialized with the configured application services and settings.</returns>
+    public DalamudApplication Build()
+    {
+        return new(_hostApplicationBuilder.Build());
+    }
+
+    private static void AddDefaultServices(IDalamudPluginInterface pluginInterface, IServiceCollection services)
+    {
+        services.AddSingleton(pluginInterface.GetRequiredService<IChatGui>());
+    }
+}
---
+++ src/Plugin.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the Archipendium Contributors under one or more agreements.
+// The Archipendium Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Archipendium.Core.Hosting;
+using Dalamud.Plugin;
+
+namespace Archipendium;
+
+/// <summary>
+/// Represents a Dalamud plugin that manages its application lifecycle and integrates with the Dalamud plugin
+/// environment.
+/// </summary>
+public class Plugin : IDalamudPlugin
+{
+    private readonly DalamudApplication _app;
+
+    /// <summary>
+    /// Initializes a new instance of the Plugin class using the specified Dalamud plugin interface.
+    /// </summary>
+    /// <param name="pluginInterface">The interface provided by Dalamud for interacting with the plugin environment. Cannot be null.</param>
+    public Plugin(IDalamudPluginInterface pluginInterface)
+    {
+        var builder = DalamudApplication.CreateBuilder(pluginInterface);
+
+        _app = builder.Build();
+
+        _app.Start();
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        _app.Stop();
+
+        GC.SuppressFinalize(this);
+    }
+}

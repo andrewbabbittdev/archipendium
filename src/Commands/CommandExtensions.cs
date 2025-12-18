9830b2912c582bd435366e113399a7ca7fbe7236
Andrew Babbitt <andrew.babbitt@outlook.com>
2025-12-18 04:51:18 -0700

    chore: separate Window and Command extensions from Core

---
+++ src/Commands/CommandExtensions.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the Archipendium Contributors under one or more agreements.
+// The Archipendium Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Dalamud.Hosting;
+
+namespace Archipendium.Commands;
+
+/// <summary>
+/// Provides extension methods for configuring Archipendium commands in a Dalamud application builder.
+/// </summary>
+public static class CommandExtensions
+{
+    /// <summary>
+    /// Configures the application builder to support Archipendium commands.
+    /// </summary>
+    /// <param name="builder">The application builder to configure. Must not be null.</param>
+    /// <returns>The same <see cref="DalamudApplicationBuilder"/> instance so that additional configuration calls can be chained.</returns>
+    public static DalamudApplicationBuilder ConfigureArchipendiumCommands(this DalamudApplicationBuilder builder)
+    {
+        return builder.AddCommands<RootCommand>();
+    }
+}
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -2,9 +2,10 @@
 // The Archipendium Contributors licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Archipendium.Commands;
 using Archipendium.Configuration;
-using Archipendium.Core;
 using Archipendium.Services;
+using Archipendium.Windows;
 using Dalamud.Hosting;
 using Dalamud.Plugin;
 
@@ -31,7 +32,8 @@ public class Plugin : IDalamudPlugin
 
         builder.ConfigureArchipendiumConfiguration()
             .ConfigureArchipendiumServices()
-            .ConfigureCore();
+            .ConfigureArchipendiumWindows()
+            .ConfigureArchipendiumCommands();
 
         App = builder.Build();
 

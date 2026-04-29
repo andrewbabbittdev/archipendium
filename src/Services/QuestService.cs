0cf4e45cdc4830f81f43763f2f8d095dbc3886f7
Andrew Babbitt <andrew.babbitt@outlook.com>
2026-04-29 16:22:13 -0600

    fix: build

--- src/Services/QuestService.cs
+++ src/Services/QuestService.cs
@@ -4,7 +4,6 @@
 
 using Archipendium.Configuration;
 using Archipendium.Services;
-using Dalamud.Game.Chat;
 using Dalamud.Game.Text;
 using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Plugin.Services;
@@ -113,7 +112,7 @@ public partial class QuestService : IHostedService, IDisposable
         GC.SuppressFinalize(this);
     }
 
-    private void OnChatMessage(IHandleableChatMessage message)
+    private void OnChatMessage(Dalamud.Game.Chat.IHandleableChatMessage message)
     {
         if (_blacklistedChatTypes.Contains(message.LogKind))
         {
